eefddbdedb5b3868e4c829064ffaf0333a97c7a1
LeeHyunSu <76948939+hyunsu4020@users.noreply.github.com>
2023-07-31 10:23:04 +0900

    대소문자 바꿔서 출력하기
    대소문자 바꿔서 출력하기
    문제 설명
    영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해 보세요.
    
    제한사항
    1 ≤ str의 길이 ≤ 20
    str은 알파벳으로 이루어진 문자열입니다.
    입출력 예
    입력 #1
    
    aBcDeFg
    출력 #1
    
    AbCdEfG

--- 0 Level/181949.py
+++ 0 Level/181949.py
@@ -6,4 +6,3 @@ for i in str:   # str 리스트를 하나씩 반복해서 받음
     else:   # i 가 소문자이면 실행
         a.append(i.upper()) # i를 대문자로 변환해서 a 리스트에 추가
 print(("").join(a)) # join함수를 사용해서 list를 str로 변환
-            
