50617f69e41cedf18c740da11e7161f815c0731b
LeeHyunSu <76948939+hyunsu4020@users.noreply.github.com>
2023-07-31 16:48:13 +0900

    최대공약수와 최소공배수
    문제 설명
    두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다. 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
    
    제한 사항
    두 수는 1이상 1000000이하의 자연수입니다.
    입출력 예
    n	m	return
    3	12	[3, 12]
    2	5	[1, 10]
    입출력 예 설명
    입출력 예 #1
    위의 설명과 같습니다.
    
    입출력 예 #2
    자연수 2와 5의 최대공약수는 1, 최소공배수는 10이므로 [1, 10]을 리턴해야 합니다.

---
+++ 1 Level/12940.py
@@ -0,0 +1,14 @@
+def solution(n, m):
+    answer = [] # answer 리스트 변수 생성
+    
+    # 최대공약수
+    for i in range(min(n, m), 0, -1):   # n과 m 중 작은 값 부터 0까지 -1하나씩 i값에 반복
+        if n % i == 0 and m % i == 0:   
+            answer.append(i)
+            break
+    # 최대 공배수
+    for i in range(max(n, m), n*m+1):   # n과 m 중 큰 값 부터 n*m 값까지 i값에 반복
+        if i % n == 0 and i % m == 0:
+            answer.append(i)
+            break
+    return answer
